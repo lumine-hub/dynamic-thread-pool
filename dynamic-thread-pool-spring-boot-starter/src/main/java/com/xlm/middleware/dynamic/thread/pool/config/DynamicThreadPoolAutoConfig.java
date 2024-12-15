a7816f009c2d81a13c56ece11cf382f56c6d1bf0
xuluming <xuluming@megaview.com>
2024-12-15 14:07:39 +0800

    feat: init project

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/xlm/middleware/dynamic/thread/pool/config/DynamicThreadPoolAutoConfig.java
@@ -0,0 +1,8 @@
+package com.xlm.middleware.dynamic.thread.pool.config;
+
+/**
+ * @author xlm
+ * 2024/12/15 下午2:00
+ */
+public class DynamicThreadPoolAutoConfig {
+}
---
+++ dynamic-thread-pool-spring-boot-starter/src/test/java/com/xlm/middleware/dynamic/thread/pool/APITest.java
@@ -0,0 +1,8 @@
+package com.xlm.middleware.dynamic.thread.pool;
+
+/**
+ * @author xlm
+ * 2024/12/15 下午2:03
+ */
+public class APITest {
+}
